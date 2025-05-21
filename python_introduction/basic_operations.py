be16da92bf37eb0a326682ae686e4e4b11af31af
SydneyGift <sydgift01@gmail.com>
2025-05-21 13:57:50 -0400

    Initial commit for python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Assign variables to user input
+
+number_1 = int(input("Enter the first number"))
+number_2 = int(input("Enter the second number"))
+
+#Assign variable to various arithmetic operations
+
+sum = number_1 + number_2
+difference = number_1 - number_2
+product = number_1 * number_2
+
+#Display the outputs of the operation
+
+print("The sum of", number_1, "and", number_2, "is", sum)
+print("The difference of", number_1, "and", number_2, "is", difference)
+print("The product of", number_1, "and", number_2, "is", product)
