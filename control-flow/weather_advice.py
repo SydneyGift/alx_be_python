1ba8dd1ab7f3b8a3829f13ec2aca7b5b3581dc3f
SydneyGift <sydgift01@gmail.com>
2025-05-29 21:55:40 -0400

    Changed variable name from current_weather to weather in weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,15 @@
 #App to provide clothing recommendations to user based on user input
 #Suggest weather conditions from input
 
-current_weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
+weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
 
 #Assign suggestions based on weather conditions
 
-if current_weather == str("sunny"):
+if weather == str("sunny"):
     print("Wear a t-shirt and sunglasses.")
-elif current_weather == str("rainy"):
+elif weather == str("rainy"):
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather == str("cold"):
+elif weather == str("cold"):
     print("Make sure to wear a warm coat and a scarf")
 else:
     print("Sorry, I dont have recommendations for this weather.")
