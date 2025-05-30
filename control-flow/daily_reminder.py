de270c3e80e5f967010c6ce152f93a84609d738b
SydneyGift <sydgift01@gmail.com>
2025-05-30 13:19:04 -0400

    Changed focus from time to priority in daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,26 +7,31 @@ time_bound = input("Is it time-bound? (yes/no):").lower()
 
 #Use match case to cater for various scenarios based on if they are time bound or not.
 
-match time_bound:
-    case "yes":
-        while time_bound == "yes":
-            if priority == "high":
+match priority:
+    case "high":
+        while priority == "high":
+            if time_bound == "yes":
                 print(f"{task} is a high priority task that requires immediate attention today!")
-            elif priority == "medium":
-                print(f"{task} is a medium priority task that requires immediate attention today!")
-            elif priority == "low":
-                print(f"{task} is a low priority task that requires immediate attention today!")
-            else:
-                print("Incorrect input for priority")
-            break
-    case "no":
-        while time_bound == "no":
-            if priority == "high":
+            elif time_bound == "no":
                 print(f"{task} is a high priority task. Consider completing it when you have free time.")
-            elif priority == "medium":
+            else:
+                print("Incorrect input for time bound")
+            break
+    case "medium":
+        while priority == "high":
+            if time_bound == "yes":
+                print(f"{task} is a medium priority task that requires immediate attention today!")
+            elif time_bound == "no":
                 print(f"{task} is a medium priority task. Consider completing it when you have free time.")
-            elif priority == "low":
+            else:
+                print("Incorrect input for time bound")
+            break
+    case "low":
+        while priority == "high":
+            if time_bound == "yes":
+                print(f"{task} is a low priority task that requires immediate attention today!")
+            elif time_bound == "no":
                 print(f"{task} is a low priority task. Consider completing it when you have free time.")
             else:
-                print("Incorrect input for priority")
+                print("Incorrect input for time bound")
             break
