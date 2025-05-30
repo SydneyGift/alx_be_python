f656d438f6eec62ffeae5904df892c3c8002626e
SydneyGift <sydgift01@gmail.com>
2025-05-29 21:43:05 -0400

    initiall commit for wather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,6 @@
+#App to provide clothing recommendations to user based on user input
+#Define weather conditions within an if statement
+
+current_weather = str(input("What's the weather like today?"))
+if current_weather = str("sunny"):
+    
