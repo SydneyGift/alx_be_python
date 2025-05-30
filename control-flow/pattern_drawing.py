3c1eea278335834eb827b132ab2e74913184288f
SydneyGift <sydgift01@gmail.com>
2025-05-30 10:29:40 -0400

    Corrected semicolon to correlate with checker

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,6 @@
 #Draw a square pattern dependent on the number the user inputs
 
-size = int(input("Enter the size of the pattern"))
+size = int(input("Enter the size of the pattern:"))
 
 while size > 0:
     range_size = size + 1
