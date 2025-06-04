1b0495cf1dd6cd39fc605171ef4a37a3eb71986a
SydneyGift <sydgift01@gmail.com>
2025-06-04 09:52:38 -0400

    Added dictionary_practice.py

---
+++ practice_projects/dictionary_practice.py
@@ -0,0 +1,9 @@
+#Programe to store information about my favourite book 
+#No neeed for user input
+title = "The Daily Stoic"
+author = "Ryan Holiday"
+genre = "Philosophy"
+
+book_details = dict(Title = title, Author = author, Genre = genre)
+
+print(book_details)
