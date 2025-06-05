6662d6aee7ca44deec25df636837e98d23cc6740
SydneyGift <sydgift01@gmail.com>
2025-06-05 07:53:48 -0400

    Added a package, 'calculator_system' with modules in it to practice creating and using modules

---
+++ practice_projects/calculator_system/calculator.py
@@ -0,0 +1,10 @@
+#Module containing functions for basic arithmetic operations
+
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def divide(x,y):
+    return x / y
+def multiply(x,y):
+    return x * y
---
+++ practice_projects/calculator_system/main.py
@@ -0,0 +1,19 @@
+import calculator
+number1 = 2
+number2 = 5
+
+#Test for addition
+sum = calculator.add(number1, number2)
+print(sum)
+
+#Test for subtraction
+subtraction = calculator.subtract(number1, number2)
+print(subtraction)
+
+#Test for division
+division = calculator.divide(number1, number2)
+print(division)
+
+#Test for multiplication
+multiplication = calculator.multiply(number1, number2)
+print(multiplication)
