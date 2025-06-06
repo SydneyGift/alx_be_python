0c7934315dfcff36c790635453a67101e5992d8f
SydneyGift <sydgift01@gmail.com>
2025-06-06 07:53:12 -0400

    optimized perform_operations.py to remove nested function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,9 @@
-def perform_operation():
+#Define arithmetic operations 
+def perform_operation(num1, num2, operations):
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
 #Define the operation parameters
-    def operation(operation_to_be_performed):
+    def operations(operation_to_be_performed):
         operation_to_be_performed = str(input("Which operation would you like to perform? (add, subtract, multiply, divide)")).strip().lower
         match operation_to_be_performed:
             case 'add':
