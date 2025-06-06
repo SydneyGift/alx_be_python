f4ae27fe77c8d2fc57e9ab15680800e18d17bd5d
SydneyGift <sydgift01@gmail.com>
2025-06-06 10:02:38 -0400

    changed from datetime.today to datetime.now in explore_date_time.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 #Define a function to display current date and time
 def display_current_datetime():
-    current_date = datetime.datetime.today()#Collecting the full date and time
+    current_date = datetime.datetime.now()#Collecting the full date and time
     #Display the current date and time
     print("Current date and time: ", current_date.strftime("%Y-%m-%d, %H:%M:%S"))
 
@@ -15,5 +15,5 @@ number_of_days = int(input("Enter the number of days to add to the current date:
 #Convert the user input into a duration
 added_days = datetime.timedelta(days = number_of_days)
 #Calculate the future date
-future_date = datetime.date.today() + added_days#Using the date only
+future_date = datetime.date.now() + added_days#Using the date only
 print("Future  date: ", future_date.strftime('%Y-%m-%d'))
