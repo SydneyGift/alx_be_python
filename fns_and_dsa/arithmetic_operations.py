618c6eb4682d819a84fee97b4793c05e2ae2d139
SydneyGift <sydgift01@gmail.com>
2025-06-06 07:46:25 -0400

    Added perform_operations.py module that defines arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+#Define the operation parameters
+    def operation(operation_to_be_performed):
+        operation_to_be_performed = str(input("Which operation would you like to perform? (add, subtract, multiply, divide)")).strip().lower
+        match operation_to_be_performed:
+            case 'add':
+                result = num1 + num2
+            case 'subtract':
+                result = num1 - num2
+            case 'multiply':
+                result = num1 * num2
+            case 'divide':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    print("Cannot divide a number by zero")
