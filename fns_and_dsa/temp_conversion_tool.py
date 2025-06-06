44a35801882f714c4bd8ba056e37cb78df236ffe
SydneyGift <sydgift01@gmail.com>
2025-06-06 13:24:33 -0400

    Added temp_conversion_tool.py

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,27 @@
+#Script to convert temperature from degrees fahrenheit to celsius and vice versa
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+#Function to convert degrees fahrenheit to degrees celsius
+def convert_to_celsius(temperature):
+    global celsius
+    celsius = temperature * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+#Function to convert degrees celcius to fahrenheit
+def convert_to_fahrenheit(temperature):
+    global fahrenheit
+    fahrenheit = temperature * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return fahrenheit
+
+temperature_input = float(input("Enter the temperature to convert: "))
+temperature_measurement = input("Is this temperature in Celsius or Fahrenheit? (C/F):").upper()
+
+if temperature_measurement == 'C':
+    convert_to_fahrenheit(temperature_input)
+    print(f"{temperature_input}°C is {fahrenheit}")
+elif temperature_measurement == 'F':
+    convert_to_celsius(fahrenheit)
+    print(f"{temperature_input}°F is {celsius}")
+else:
+    print("Invalid temperature measurement selection")
