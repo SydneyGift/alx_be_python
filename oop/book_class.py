98fda65a6e194a20106091115a846d8758009c55
SydneyGift <sydgift01@gmail.com>
2025-06-20 12:14:02 -0400

    initial commit

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+#program with a Book class that uses specific magic methods to enhance its functionality. 
+class Book:
+    def __init__(self, title, author, year):
+      self.title = title
+      self.author = author
+      self.year = year
+
+#Display this information with a string representation
+    def __str__(self):
+       return f"({self.title}) by ({self.author}), published in ({self.year})"
+
+#Dislplay this information with official representation
+    def __repr__(self):
+       return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+#Usee a destructor for object deletion
+    def __del__(self):
+       return f"Deleting ({self.title})"
