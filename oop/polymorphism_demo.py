9e36d138b17a3b982da1fbbbfb134634b491b404
SydneyGift <sydgift01@gmail.com>
2025-06-20 14:06:46 -0400

    Initial commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+from math import pi
+class Shape:
+    def __init__(self):
+        pass
+    def area (self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area (self):
+        area = self.length * self.width
+        return area
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        area = pi * self.radius * self.radius
+        return area
